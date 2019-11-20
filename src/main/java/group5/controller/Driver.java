bc277ade087be45c94630a986d4dfafd24430f4b
Pavdrew <pavloandrew@gmail.com>
2019-11-19 18:39:34 -0700

    database connection and bulk insert methods done

--- src/main/java/group5/controller/Driver.java
+++ src/main/java/group5/controller/Driver.java
@@ -1,8 +1,12 @@
 package group5.controller;
 
+import group5.model.Team;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
 public class Driver {
 
@@ -17,6 +21,31 @@ public class Driver {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection("jdbc:sqlserver://localhost;databaseName=sport_teams;user=lab2;password=lab2");
         } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void bulkInsertTeams(List<Team> teams) {
+        String sql = "INSERT INTO sport_teams (team_name, team_location, sport_type) " + "VALUES (?,?,?)";
+        int teamCounter = 0;
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            for (Team team : teams) {
+                statement.setString(1, team.getTeamName());
+                statement.setString(2, team.getTeamLocation());
+                statement.setString(3, team.getSportType());
+
+                statement.addBatch();
+                teamCounter++;
+
+                if(teamCounter > 65000) {
+                    statement.executeBatch();
+                    teamCounter = 0;
+                }
+            }
+            statement.executeBatch();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
